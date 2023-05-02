c04261075a4af9dd4666138b319715302bb458de
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2023-05-02 18:52:07 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        long A = scanner.nextLong();
+        long B = scanner.nextLong();
+        long C = scanner.nextLong();
+        System.out.println(A+B+C);
+    }
+}
