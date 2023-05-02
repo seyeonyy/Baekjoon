873cc48405364594883bd04fae8b1f88597f5be5
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2023-05-02 18:50:43 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String id = scanner.next();
+        System.out.println(id+"??!");
+    }
+}
