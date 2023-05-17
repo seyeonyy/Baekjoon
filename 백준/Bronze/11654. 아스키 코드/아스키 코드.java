700b07c2e00c7f3f794633bc793485ccf31361ba
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2023-05-18 01:10:02 +0900

    [Bronze V] Title: 아스키 코드, Time: 212 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char ascii = scanner.next().charAt(0);
+        int ascii_num = (int)ascii;
+        System.out.println(ascii_num);
+    }
+}
